9a4fce231a7a0c65b6a18b27786478640a2b5fa3
Bryan <bryan85.le@outlook.com>
2023-11-05 11:34:00 +1300

    ok

--- pages/dashtoday/farmtoday.py
+++ pages/dashtoday/farmtoday.py
@@ -1,4 +1,4 @@
-#https://www.afimilk.com/docs/documents/afifarm/anestrus%20report.htm?tocpath=_____5
+# https://www.afimilk.com/docs/documents/afifarm/anestrus%20report.htm?tocpath=_____5
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 import pandas as pd
@@ -8,20 +8,14 @@ from dash.exceptions import PreventUpdate
 
 from componentcallbacks import Farmtoday
 
-register_page(
-    __name__,
-    path = '/farmtoday',
-    title = 'Farmer Today report'
-)
+register_page(__name__, path="/farmtoday", title="Farmer Today report res")
 
 
-
-
-content = dmc.Title('I am here')
+content = dmc.Title("I am here")
 
 layout = Farmtoday(
-    children = content,
-    id = 'farm-report-test',
+    children=content,
+    id="farm-report-test",
 )
 
 
