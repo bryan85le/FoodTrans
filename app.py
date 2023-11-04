7a7d9f8061513fec02dea5a9df3aa62b7718b2d8
Bryan <bryan85.le@outlook.com>
2023-11-05 10:22:36 +1300

    ok

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 # Native Python packages
 import logging
 from socket import gethostname
@@ -32,28 +31,72 @@ from components import (
 
 # Create server with secret key
 server = Flask(__name__)
-server.secret_key = 'SECRET_KEY'
-
+server.secret_key = "SECRET_KEY"
 
 
 # Instanciate Dash app
 app = Dash(
-    name = __name__,
-    server = server,
-    title = 'NZ Hyper',
-    use_pages = True,
-    update_title = 'Updating...',
+    name=__name__,
+    server=server,
+    title="NZ Hyper",
+    use_pages=True,
+    update_title="Updating...",
 )
+
+# Instanciate Error Handler
+file_handler = logging.FileHandler("data/errorlog.log")
+file_handler.setLevel(logging.WARNING)
+server.logger.addHandler(file_handler)
+
+
+# Create Drawer
+nav_links = [
+    DrawerSingleItem(name="Login", icon="bx:log-in", href="/login"),
+    DrawerSingleItem(name="Page 1", icon="bx:scatter-chart", href="/page1"),
+    DrawerMultiItem(
+        name="Page2",
+        icon="bx:line-chart",
+        href="/page2/subpage1",
+        submenu=[
+            DrawerSubItem(name="Subpage 1", href="/page2/subpage1"),
+            DrawerSubItem(name="Subpage 2", href="/page2/subpage2"),
+        ],
+    ),
+    DrawerFooter(title="Footer", subtitle="Footer Subtitle"),
+]
+
+
+# Create a global data excell
+import pandas as pd
+
+df = pd.read_csv("data/hyperFarm.csv")
+
+InitData = html.Div([dcc.Store(id="global-data"), html.Div(id="dump-inputdata")])
+
+
+@callback(
+    Output("global-data", "data"),
+    Input("dump-inputdata", "children"),
+    State("global-data", "data"),
+    prevent_initial_call=False,
+)
+def filter_countries(_, data):
+    if data is None:
+        # raise PreventUpdate
+        return df.to_dict("records")
+    return data
 
 
 # Create Dashboard Layout
-app.layout = dash.page_container
-
+app.layout = Dashboard(
+    children=page_container,
+    id="dashboard",
+    navbar=Navbar(title="My Web App", id="dashboard-navbar"),
+    drawer=Drawer(menu=nav_links, logo_name="Charlotte", logo_icon="heroicons:rocket-launch-20-solid"),
+    initialdata=InitData,
+)
 
 
 # Run app
-if __name__ == '__main__':
-    app.run(
-        host = '0.0.0.0',
-        port = 5000
-    )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
