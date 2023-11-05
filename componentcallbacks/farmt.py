1e4b762faf21094a9111b0b3c0926409bc2524b6
Bryan <bryan85.le@outlook.com>
2023-11-06 11:05:50 +1300

    o

--- componentcallbacks/farmt.py
+++ componentcallbacks/farmt.py
@@ -6,6 +6,7 @@ from typing import Optional
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 import pandas as pd
+import plotly.express as px
 from dash import MATCH, Input, Output, State, callback, dash_table, dcc, html
 from dash.development.base_component import Component
 from dash_bootstrap_components import Container
@@ -21,6 +22,9 @@ class FarmToday(Container):
     id : str, default='health-report'
         Dashboard ID.
     """
+
+    df = px.data.tips()
+    fig = px.pie(df, values="tip", names="day")
 
     def __init__(
         self,
@@ -60,6 +64,78 @@ class FarmToday(Container):
                         [
                             html.H3("Health"),
                             tlbContent(title="Mastitis"),
+                            tlbContent(title="Degestion Problems"),
+                            tlbContent(title="Ketosis"),
+                            tlbContent(title="Fresh cows to check"),
+                            tlbContent(title="Non-specific health report"),
+                        ]
+                    ),
+                ],
+                # style={"width": "20rem"},
+            ),
+            dbc.Card(
+                [
+                    dbc.CardImg(src="/assets/img/register.svg", top=True),
+                    dbc.CardBody(
+                        [
+                            html.H3("Groups"),
+                            tlbContent(title="Welfare alerts(in groups)"),
+                            tlbContent(title="Nutrition alerts"),
+                        ]
+                    ),
+                ],
+                # style={"width": "20rem"},
+            ),
+            dbc.Card(
+                [
+                    dbc.CardImg(src="/assets/img/register.svg", top=True),
+                    dbc.CardBody(
+                        [
+                            html.H3("Todo to today"),
+                            tlbContent(title="To dry today"),
+                            tlbContent(title="Due calving"),
+                            tlbContent(title="Sorted Animals"),
+                            tlbContent(title="For pregnacny check"),
+                        ]
+                    ),
+                ],
+                # style={"width": "20rem"},
+            ),
+            dbc.Card(
+                [
+                    dbc.CardImg(src="/assets/img/register.svg", top=True),
+                    dbc.CardBody(
+                        [
+                            html.H3("Faults today"),
+                            tlbContent(title="Un-Assigned Tags"),
+                            tlbContent(title="Not Identified in Milking"),
+                            tlbContent(title="In Milking"),
+                            tlbContent(title="Wrong Group"),
+                        ]
+                    ),
+                ],
+                # style={"width": "20rem"},
+            ),
+            dbc.Card(
+                [
+                    dbc.CardImg(src="/assets/img/register.svg", top=True),
+                    dbc.CardBody(
+                        [
+                            html.H3("Animals Invetory"),
+                            tlbContent(title="Cows"),
+                            tlbContent(title="Pregnant Milk"),
+                            tlbContent(title="Open"),
+                            tlbContent(title="Dry"),
+                        ]
+                    ),
+                ],
+                # style={"width": "20rem"},
+            ),
+            dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+                            dcc.Graph(figure=self.fig),
                         ]
                     ),
                 ],
